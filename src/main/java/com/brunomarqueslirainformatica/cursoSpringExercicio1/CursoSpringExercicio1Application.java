1847cdbe4aa52366438701cf874f6dcd273572fe
Bruno Marques Lira <brunomarqueslira@outlook.com>
2020-05-25 17:33:33 -0300

    Criação do Projeto

---
+++ src/main/java/com/brunomarqueslirainformatica/cursoSpringExercicio1/CursoSpringExercicio1Application.java
@@ -0,0 +1,13 @@
+package com.brunomarqueslirainformatica.cursoSpringExercicio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringExercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringExercicio1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunomarqueslirainformatica/cursoSpringExercicio1/CursoSpringExercicio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunomarqueslirainformatica.cursoSpringExercicio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringExercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
