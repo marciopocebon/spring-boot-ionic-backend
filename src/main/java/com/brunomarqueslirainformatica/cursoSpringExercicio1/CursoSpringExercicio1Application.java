acab02ad12eeadb27fedd1c6d40f66de51d55e82
Bruno Marques Lira <brunomarqueslira@outlook.com>
2020-05-26 10:22:17 -0300

    Criando operacao de instanciacao

--- src/main/java/com/brunomarqueslirainformatica/cursoSpringExercicio1/CursoSpringExercicio1Application.java
+++ src/main/java/com/brunomarqueslirainformatica/cursoSpringExercicio1/CursoSpringExercicio1Application.java
@@ -1,13 +1,31 @@
 package com.brunomarqueslirainformatica.cursoSpringExercicio1;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoSpringExercicio1Application {
+import com.brunomarqueslirainformatica.cursoSpringExercicio1.domain.Categoria;
+import com.brunomarqueslirainformatica.cursoSpringExercicio1.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoSpringExercicio1Application implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringExercicio1Application.class, args);
+	}
+	
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
